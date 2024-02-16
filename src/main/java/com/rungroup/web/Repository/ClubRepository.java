cd86b033daa11cc7aeed642ac177a2c23d97b380
Luis David <david12.29@hotmail.com>
2024-02-16 09:59:59 -0500

    Create ClubDto class)

--- src/main/java/com/rungroup/web/Repository/ClubRepository.java
+++ src/main/java/com/rungroup/web/Repository/ClubRepository.java
@@ -1,4 +1,11 @@
 package com.rungroup.web.Repository;
 
-public class ClubRepository {
+import com.rungroup.web.Models.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClubRepository extends JpaRepository<Club,Long> {
+    Optional<Club> findByTitle (String url);
+
 }
---
+++ src/main/java/com/rungroup/web/dto/ClubDto.java
@@ -0,0 +1,4 @@
+package com.rungroup.web.dto;
+
+public class ClubDto {
+}
