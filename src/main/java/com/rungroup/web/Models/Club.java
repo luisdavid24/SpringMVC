a531c9626418b5e6f43ffe37b76ff264334511ee
Luis David <david12.29@hotmail.com>
2024-02-16 07:49:08 -0500

    Update Club class

--- src/main/java/com/rungroup/web/Models/Club.java
+++ src/main/java/com/rungroup/web/Models/Club.java
@@ -1,4 +1,14 @@
 package com.rungroup.web.Models;
 
+import java.time.LocalDateTime;
+
 public class Club {
+
+    private Long id;
+    private String title;
+    private String photoUrl;
+    private String content;
+    private LocalDateTime createOn;
+    private LocalDateTime updateOn;
+
 }
